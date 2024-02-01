089667f6f8b7156585319184c6d1a6dc89ad5975
Dava Net <grandmaster22889@gmail.com>
2024-02-01 21:42:07 +0300

    Test_commit

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,4 @@
-FROM openjdk:17-jdk-alpine
-
-EXPOSE 8080
-
-ADD target/ROOT.jar ROOT.jar
-
-CMD ["java", "-jar", "ROOT.jar"]
+FROM openjdk:17-jdk-slim
+EXPOSE 8081
+ADD target/ConditionalApp-0.0.1-SNAPSHOT.jar myapp.jar
+ENTRYPOINT ["java","-jar","/myapp.jar"]
--- src/test/java/ru/netology/REST_homework/RestHomeworkApplicationTests.java
+++ src/test/java/ru/netology/REST_homework/RestHomeworkApplicationTests.java
@@ -1,13 +1,42 @@
 package ru.netology.REST_homework;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class RestHomeworkApplicationTests {
+	@Autowired
+	TestRestTemplate restTemplate;
+
+	@Container
+	private static final GenericContainer<?> testContDev = new GenericContainer<>("devapp")
+			.withExposedPorts(8080);
+	@Container
+	private static final GenericContainer<?> testContProd = new GenericContainer<>("prodapp")
+			.withExposedPorts(8081);
 
 	@Test
-	void contextLoads() {
+	void contextLoadsDev() {
+		int devContainerPort = testContDev.getMappedPort(8080);
+		ResponseEntity<String> forDevEntity = restTemplate.getForEntity("http://localhost:" + devContainerPort + "/profile", String.class);
+		String devExpected = forDevEntity.getBody();
+		Assertions.assertEquals(devExpected, "Current profile is dev");
 	}
 
+	@Test
+	void contextLoadsProd() {
+		int prodContainerPort = testContProd.getMappedPort(8081);
+		ResponseEntity<String> forProdEntity = restTemplate.getForEntity("http://localhost:" + prodContainerPort + "/profile", String.class);
+		String prodExpected = forProdEntity.getBody();
+		Assertions.assertEquals(prodExpected, "Current profile is production");
+	}
 }
